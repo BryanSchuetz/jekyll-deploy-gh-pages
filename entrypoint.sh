aaedf05fca1c5bf4c7a6b1b4aa6381dfc0a4f487
Bryan Schuetz <bschuetz@gmail.com>
2019-10-04 13:45:39 -0400

    using x-access-token

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo '👍 BUNDLE INSTALLED—BUILDING THE SITE'
 bundle exec jekyll build
 echo '👍 THE SITE IS BUILT—PUSHING IT BACK TO GITHUB-PAGES'
 cd build
-remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+remote_repo="https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
 remote_branch="gh-pages" && \
 git init && \
 git config user.name "${GITHUB_ACTOR}" && \
